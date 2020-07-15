d69403e007e6f475dc377b41086e600ecc9fe636
Dawood Khan <dawoodkhan82@gmail.com>
2020-07-15 12:09:18 -0400

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -20,5 +20,8 @@ gr.Interface(
 	predict, 
 	sketchpad,  # could also be 'sketchpad' 
 	label,
+	title="MNIST Fashion Sketch Pad",
+        description="This model is trained on the widely known MNIST fashion dataset. Draw an article of clothing and see if the model can guess what it is",
+        thumbnail="https://3qeqpr26caki16dnhd19sv6by6v-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/Plot-of-a-Subset-of-Images-from-the-Fashion-MNIST-Dataset-1024x768.png",
 	capture_session=True,
 	live=True).launch();
